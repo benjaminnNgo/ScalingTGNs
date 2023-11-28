10c07c0a4ae251d1f9c6b06ae2c2c9cd2c685c9f
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2023-11-28 10:22:56 -0500

    updating script

--- discretize_ctdg.py
+++ discretize_ctdg.py
@@ -11,7 +11,7 @@ from utils import *
 #! load the datasets
 # dataset = tgx.builtin.uci()
 
-data_name = "tgbl-wiki"
+data_name = "tgbl-review" #"tgbl-wiki"
 dataset = tgx.tgb_data(data_name)
 # dataset = tgx.tgb_data("tgbl-review")
 # dataset = tgx.tgb_data("tgbl-coin") 
@@ -20,11 +20,11 @@ dataset = tgx.tgb_data(data_name)
 ctdg = tgx.Graph(dataset)
 # ctdg.save2csv("ctdg")
 
-time_scale = "hourly"
+time_scale = "monthly" #"weekly" #"daily"  #"hourly" #"minutely" 
 dtdg, ts_list = ctdg.discretize(time_scale=time_scale, store_unix=True)
 print ("discretize to ", time_scale)
 print ("there is time gap, ", dtdg.check_time_gap())
-list2csv(ts_list, data_name + "_ts.csv")
+list2csv(ts_list, data_name + "_ts" + "_" + time_scale + ".csv")
 
 
 
