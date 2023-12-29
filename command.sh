ba095046a741d35634244c852392360de5c29d50
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2023-12-29 12:52:26 -0500

    adding command to generate all ns samples

---
+++ command.sh
@@ -0,0 +1,5 @@
+python tgbl-wiki_ns_gen.py -d tgbl-review -t daily
+python tgbl-wiki_ns_gen.py -d tgbl-review -t monthly
+python tgbl-wiki_ns_gen.py -d tgbl-review -t hourly
+python tgbl-wiki_ns_gen.py -d tgbl-review -t minutely
+
